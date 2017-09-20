a5b0a8c4b1cbb665534f660ecad372a056f72d4a
Ajay Mishra <ajay3.mishra@airtel.com>
2017-09-20 15:26:09 +0530

    in progress

--- mobile-easy-shopping-server/src/main/java/com/mob/shopping/controller/CustomerController.java
+++ mobile-easy-shopping-server/src/main/java/com/mob/shopping/controller/CustomerController.java
@@ -1,14 +1,8 @@
 package com.mob.shopping.controller;
 
-import com.mob.shopping.beans.ResponseBean;
-import com.mob.shopping.beans.request.CheckBankStatusRequest;
-import com.mob.shopping.beans.request.RetailerBalanceRequest;
-import com.mob.shopping.service.RetailerServices;
-import org.apache.camel.ProducerTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
